f0879e9dd90f8792400c4c855ac33b1615758150
Anto Martínez <antomartinezdev@gmail.com>
2024-03-12 16:59:15 +0100

    Unit 5 - I update game manager to update the text score in UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targetPrefabs;
+    public TextMeshProUGUI scoreText;
 
     private float _spawnRate = 1f;
+    private int _score;
 
     void Start(){
+        _score = 0;
+        UpdateScoreText(_score);
         StartCoroutine(SpawnTarget());
     }
 
@@ -18,5 +23,10 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targetPrefabs.Count);
             Instantiate(targetPrefabs[index]);
         }
+    }
+
+    public void UpdateScoreText(int scoreToAdd){
+        _score += scoreToAdd;
+        scoreText.text = "Score: " + _score;
     }
 }
