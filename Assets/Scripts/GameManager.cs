cbe86868d64a7edde9b885b10150a8c8c8f0f4d1
Anto Martínez <antomartinezdev@gmail.com>
2024-03-12 17:13:46 +0100

    Unit 5 - I add restart game option at the end of the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targetPrefabs;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
 
     private float _spawnRate = 1f;
     private int _score;
     public bool isGameActive;
 
     void Start(){
+        isGameActive = true;
         _score = 0;
         UpdateScoreText(_score);
         StartCoroutine(SpawnTarget());
-        isGameActive = true;
     }
 
     IEnumerator SpawnTarget(){
@@ -36,5 +39,9 @@ public class GameManager : MonoBehaviour
     public void GameOver(){
         isGameActive = false;
         gameOverText.gameObject.SetActive(true);
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
