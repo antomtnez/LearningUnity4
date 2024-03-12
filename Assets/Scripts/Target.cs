cd22586cc495bdaf65b4f062b29aecb694b628ac
Anto Martínez <antomartinezdev@gmail.com>
2024-03-12 16:41:48 +0100

    Unit 5 - I update target component to detect mouse down and destroy it

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody _rb;
+
+    private float _minSpeed = 12f;
+    private float _maxSpeed = 16f;
+    private float _maxTorque = 10f;
+    private float _xRange = 4f;
+    private float _ySpawnPos = 6f;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+
+        _rb.AddForce(RandomForce(), ForceMode.Impulse);
+        _rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    Vector3 RandomForce(){
+        return Vector3.up * Random.Range(_minSpeed, _maxSpeed);
+    }
+
+    float RandomTorque(){
+        return Random.Range(-_maxTorque, _maxTorque);
+    }
+
+    Vector3 RandomSpawnPos(){
+        return new Vector3(Random.Range(-_xRange, _xRange), -_ySpawnPos);
+    }
+
+    private void OnMouseDown() {
+        Destroy(gameObject);    
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);    
     }
 }
